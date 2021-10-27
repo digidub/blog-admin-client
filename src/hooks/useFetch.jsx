f5c2d1aa122363afdb8ffa36517e418b3a073bd7
Sholto <madein.mania@gmail.com>
2021-10-27 15:08:32 +0100

    api fetch hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const useFetch = ({ query }) => {
+  const [apiKey, setApiKey] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [data, setData] = useState(null);
+
+  const apiCall = async () => {
+    setStatus('fetching');
+    const response = await fetch(query.url, query.options);
+    const responseData = await response.json();
+    setData(responseData);
+    setStatus('fetched');
+  };
+
+  useEffect(() => {
+    if (!query) return;
+    apiCall();
+  }, [query]);
+
+  return <div></div>;
+};
+
+export default useFetch;
