392c26329b90affc942b3dc479c377d75d86cc6f
Sholto <madein.mania@gmail.com>
2021-10-27 20:47:10 +0100

    extend post display

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const PostCard = (props) => {
-  return <div>{props.title}</div>;
+const PostCard = ({ body, title, author, posted }) => {
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{body}</div>
+      <div>{author}</div>
+      <div>{posted}</div>
+    </div>
+  );
 };
 
 export default PostCard;
