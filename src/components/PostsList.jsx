01795826aacb431d482e3b7dc19555abd2619d5e
Sholto <madein.mania@gmail.com>
2021-10-27 17:25:22 +0100

    working on display of posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
 
-const PostsList = () => {
-  return <div></div>;
+const PostsList = (props) => {
+  const url = 'http://localhost:3000/posts';
+  const options = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
+  const query = { url, options };
+  const { loading, error, data } = useFetch(query);
+
+  return (
+    <div>
+      {loading && <div>loading</div>}
+      {error && <div>{error}</div>}
+      {data && console.log(data)}
+    </div>
+  );
 };
 
 export default PostsList;
