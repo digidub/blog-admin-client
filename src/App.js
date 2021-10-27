446b73b443efd7d8c06d13a73a89a205d1d7a51c
Sholto <madein.mania@gmail.com>
2021-10-27 14:35:46 +0100

    basic API key login functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import Login from './components/Login';
 
 function App() {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [apiKey, setApiKey] = useState(null);
+
+  const handleUsername = (e) => {
+    setUsername((username) => e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword((password) => e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    };
+    fetch('http://localhost:3000/auth/login', options)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div className='App'>
-      <Login />
+      <Login
+        handleUsername={handleUsername}
+        handlePassword={handlePassword}
+        handleLogin={handleLogin}
+      />
     </div>
   );
 }
