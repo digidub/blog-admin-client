4eb89ed6d120a60e8c7b56e08115bac202819201
Sholto <madein.mania@gmail.com>
2021-10-27 16:19:04 +0100

    rewrote fetch hook

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,24 +1,27 @@
 import { useState, useEffect } from 'react';
 
-const useFetch = ({ query }) => {
-  const [apiKey, setApiKey] = useState(null);
-  const [status, setStatus] = useState(null);
-  const [data, setData] = useState(null);
-
-  const apiCall = async () => {
-    setStatus('fetching');
-    const response = await fetch(query.url, query.options);
-    const responseData = await response.json();
-    setData(responseData);
-    setStatus('fetched');
-  };
+const useFetch = (query) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: false,
+    data: [],
+  });
 
   useEffect(() => {
-    if (!query) return;
-    apiCall();
-  }, [query]);
+    fetch(query.url, query.options)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setState({ loading: false, error: false, data });
+      })
+      .catch((error) => setState({ loading: false, error, data: [] }));
+  }, []);
 
-  return { status, data };
+  return state;
 };
 
 export default useFetch;
