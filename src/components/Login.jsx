9c80555c056cd58f1a25da842addfec94d4509d8
Sholto <madein.mania@gmail.com>
2021-10-29 10:19:17 +0100

    styling of component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Login = ({ handleUsername, handlePassword, handleLogin }) => {
   return (
-    <div>
-      <form>
-        <label htmlFor='username'>username</label>
-        <input type='text' name='username' onChange={handleUsername} />
-        <label htmlFor='password'>password</label>
-        <input type='password' name='password' onChange={handlePassword} />
+    <LoginWrapper>
+      <h1>Please Log In</h1>
+      <LoginForm>
+        <label htmlFor='username'>Username:</label>
+        <Input type='text' name='username' onChange={handleUsername} />
+        <label htmlFor='password'>Password:</label>
+        <Input type='password' name='password' onChange={handlePassword} />
         <input type='submit' value='login' onClick={handleLogin} />
-      </form>
-    </div>
+      </LoginForm>
+    </LoginWrapper>
   );
 };
 
 export default Login;
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  margin: 5px 0 5px 0;
+`;
