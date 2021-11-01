6fa9df5061e6af7bf67c8bce747c6349fd92ad0f
Sholto <madein.mania@gmail.com>
2021-11-01 08:40:08 +0000

    rich text editor

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,31 +1,27 @@
-import React, { useState, useContext } from 'react';
-import { AuthContext } from '../AuthContext';
+import React, { useState, useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
 const NewPost = () => {
   const [editTitle, setEditTitle] = useState();
   const [editBody, setEditBody] = useState();
-  const [auth] = useContext(AuthContext);
   const [data, setData] = useState(null);
+  const editorRef = useRef(null);
 
   const handleTitleChange = (e) => {
     setEditTitle((editTitle) => e.target.value);
   };
 
-  const handleBodyChange = (e) => {
-    setEditBody((editBody) => e.target.value);
-  };
-
   const handleSaveChanges = (e) => {
     e.preventDefault();
+
     const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${auth}`,
       },
       body: JSON.stringify({ title: editTitle, body: editBody }),
     };
-    fetch(`http://localhost:3000/posts`, options)
+    fetch(`/posts`, options)
       .then((res) => res.json())
       .then((data) => console.log(data))
       .catch((err) => console.log(err));
@@ -33,22 +29,37 @@ const NewPost = () => {
 
   return (
     <div>
-      <form>
-        <label htmlFor='title'>Title:</label>
-        <input
-          type='text'
-          name='title'
-          value={editTitle}
-          onChange={handleTitleChange}
-        />
-        <label htmlFor='body'>Body:</label>
-        <textarea name='body' value={editBody} onChange={handleBodyChange} />
-        <input
-          type='submit'
-          value='save changes'
-          onClick={handleSaveChanges}
-        />
-      </form>
+      <label htmlFor='title'>Title:</label>
+      <input
+        type='text'
+        name='title'
+        value={editTitle}
+        onChange={handleTitleChange}
+      />
+      <Editor
+        apiKey={process.env.REACT_APP_TinyCloud}
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue='<p>Enter blog post here</p>'
+        value={editBody}
+        onEditorChange={(newValue, editor) => setEditBody(newValue)}
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount',
+          ],
+          toolbar:
+            'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+      <input type='submit' value='save changes' onClick={handleSaveChanges} />
     </div>
   );
 };
