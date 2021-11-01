4bd47a2a821881967386543e9754ee0fa36a0711
Sholto <madein.mania@gmail.com>
2021-11-01 09:05:52 +0000

    comment list component

---
+++ src/components/CommentsList.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import useFetch from '../hooks/useFetch';
+
+const Loading = () => <p>Loading...</p>;
+
+const Error = (error) => <p>Oops! Something went wrong: {error}</p>;
+
+const Data = (props) => {
+  console.log(props);
+  return props.data.map((comment) => {
+    return (
+      <div
+        id={comment._id}
+        key={comment._id}
+        body={comment.body}
+        email={comment.email}
+        posted={comment.datePosted}
+      />
+    );
+  });
+};
+
+const CommentsList = (props) => {
+  const url = props.url;
+  const options = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
+  const query = { url, options };
+  const { loading, error, data } = useFetch(query);
+
+  return (
+    <div>
+      {loading && <Loading />}
+      {error && <Error error={error} />}
+      {data && <Data data={data} />}
+    </div>
+  );
+};
+
+export default CommentsList;
