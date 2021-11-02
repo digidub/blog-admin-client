1fa19e9614a30d3baadf34bb18b1b76c1bba99d1
Sholto <madein.mania@gmail.com>
2021-11-02 10:01:42 +0000

    extract server communication functions

---
+++ src/services.js
@@ -0,0 +1,55 @@
+const fetchAll = (url) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
+};
+
+const create = (url, title, body) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title, body }),
+  };
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
+};
+
+const update = (url, title, body) => {
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title, body }),
+  };
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
+};
+
+const remove = (url) => {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
+};
+
+export default { fetchAll, create, update, remove };
